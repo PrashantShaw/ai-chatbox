cc7c1ed6047e95e27654f757b2ea26ed0542bb30
PrashantShaw <prashant.shaw.3154@gmail.com>
2024-07-21 02:30:59 +0530

    feat: chat like ui ~V1

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,48 @@ import { useChat } from 'ai/react';
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit, isLoading } = useChat();
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(m => (
-        <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
-          {m.content}
-        </div>
-      ))}
+    <div className="flex flex-col w-full max-w-2xl pb-24 pt-10 mx-auto stretch">
+
+      <div className="flex flex-col gap-5 ">
+        {messages.map(({ id, role, content }) => (
+          <MessageWrapper key={id} fromUser={role === 'user'} message={content} />
+        ))}
+      </div>
 
       <form onSubmit={handleSubmit}>
         <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+          className="fixed bottom-0 w-full max-w-2xl p-2 mb-8 border border-gray-300 rounded shadow-xl"
           value={input}
-          placeholder="Say something..."
+          placeholder="Ask Anything..."
           onChange={handleInputChange}
         />
       </form>
     </div>
   );
+}
+
+const UserAvatar = () => {
+  return <div className="border shrink-0 grid place-content-center h-10 w-10 rounded-full bg-purple-700 shadow-lg">
+    <p className='font-semibold text-white'>U</p>
+  </div>
+}
+const AiAvatar = () => {
+  return <div className="border shrink-0 grid place-content-center h-10 w-10 rounded-full bg-cyan-700 shadow-lg">
+    <p className='font-semibold text-white'>AI</p>
+  </div>
+}
+type MessageWrapperProps = {
+  fromUser: boolean,
+  message: string
+}
+
+const MessageWrapper = ({ fromUser, message }: MessageWrapperProps) => {
+  const wrapperClasses = fromUser ? "pl-[3.25rem] flex-row-reverse" : "pr-[3.25rem]"
+  const bubbleClasses = fromUser ? "bg-purple-50" : "bg-cyan-50"
+  return <div className={"whitespace-pre-wrap flex items-end gap-3 " + wrapperClasses}>
+    {fromUser ? <UserAvatar /> : <AiAvatar />}
+    <div className={"w-fit p-4 rounded-xl shadow " + bubbleClasses}>
+      <p className=''>{message}</p>
+    </div>
+  </div>
 }
