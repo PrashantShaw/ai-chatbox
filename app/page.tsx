a7518ce4886ae1d0bae210089b325d321a88564b
PrashantShaw <prashant.shaw.3154@gmail.com>
2024-07-22 01:48:44 +0530

    feat: scroll to view when message is generated and search bar ui fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,36 @@
 'use client';
 
 import { useChat } from 'ai/react';
+import { useRef } from 'react';
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit, isLoading } = useChat();
-  return (
-    <div className="flex flex-col w-full max-w-2xl pb-24 pt-10 mx-auto stretch">
+  const chatWrapperRef = useRef<HTMLDivElement>(null)
 
-      <div className="flex flex-col gap-5 ">
-        {messages.map(({ id, role, content }) => (
-          <MessageWrapper key={id} fromUser={role === 'user'} message={content} />
-        ))}
+  const scrollViewMessageBubble = () => {
+    if (chatWrapperRef.current) {
+      chatWrapperRef.current.scrollTo({
+        top: chatWrapperRef.current.scrollHeight,
+        behavior: 'smooth'
+      });
+    }
+  }
+
+  return (
+    <div className="flex flex-col justify-between h-screen w-full max-w-2xl pt-8 mx-auto ">
+      <div ref={chatWrapperRef} className="flex flex-col gap-5 overflow-auto px-3 py-4">
+        {messages.map(({ id, role, content }) => {
+          scrollViewMessageBubble()
+          return <MessageWrapper key={id} fromUser={role === 'user'} message={content} />
+        }
+        )}
       </div>
 
-      <form onSubmit={handleSubmit}>
+      <form className='px-3' onSubmit={(evt) => {
+        scrollViewMessageBubble()
+        handleSubmit(evt)
+      }}>
         <input
-          className="fixed bottom-0 w-full max-w-2xl p-2 mb-8 border border-gray-300 rounded shadow-xl"
+          className="w-full max-w-2xl p-2 my-8 border border-gray-300 rounded shadow-xl"
           value={input}
           placeholder="Ask Anything..."
           onChange={handleInputChange}
@@ -44,8 +60,8 @@ const MessageWrapper = ({ fromUser, message }: MessageWrapperProps) => {
   const bubbleClasses = fromUser ? "bg-purple-50" : "bg-cyan-50"
   return <div className={"whitespace-pre-wrap flex items-end gap-3 " + wrapperClasses}>
     {fromUser ? <UserAvatar /> : <AiAvatar />}
-    <div className={"w-fit p-4 rounded-xl shadow " + bubbleClasses}>
-      <p className=''>{message}</p>
+    <div id='messageWrapper' className={"w-fit p-4 rounded-xl shadow " + bubbleClasses}>
+      <p className='text-sm'>{message}</p>
     </div>
   </div>
 }
